f265664de1157aba3fc3625e61c995e7b247fa66
Paul Saf <hollysaf@yahoo.com>
2022-01-20 09:53:47 +0300

    Revert "added / /doc routes"
    This reverts commit ca4b7486fde379be7311618a2a4d1baad0776cc0.

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,32 +9,14 @@ interface CustomRoute {
     handler: DefaultRoute<CustomRequest, FastifyReply>;
 }
 
-const getLoginOpts: CustomRoute = {
-    schema: {},
-    handler: (req,reply)=>{
-        reply.send('Switch POST please')
-    }
-}
 
-const postLoginOpts: CustomRoute = {
+const loginUserOpts: CustomRoute = {
     schema: postUserSchema,
     handler: loginUser
 }
 
-const MainOpts: CustomRoute = {
-    schema: {},
-    handler: (req,reply) => {
-        reply.send('Hello, to work with API you need to login first at localhost:4000/login URL')
-    }
-}
-
 const authRoute:FastifyPluginCallback =  (server, _option, done) => {
-    server.get('/login', getLoginOpts)
-    server.post('/login', postLoginOpts);
-    server.get('/', MainOpts)
-    server.post('/', MainOpts)
-    server.post('/doc', MainOpts)
-    server.get('/doc', MainOpts)
+    server.post('/login', loginUserOpts);
     done();
 };
 

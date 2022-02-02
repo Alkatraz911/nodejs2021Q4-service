63bbcaf5ae8ffdf29a60694ba7785126517e548b
unknown <hollysaf@yahoo.com>
2022-02-02 12:26:33 +0300

    added exeptionFilter

--- rest/src/main.ts
+++ rest/src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { HttpExceptionFilter } from './helpers/exeptionFilter'
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let app;
+  let mode;
+  if (process.env.USE_FASTIFY === 'true') {
+    mode = 'fastify'
+    app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter()
+    );
+  } else {
+    mode = 'express'
+    app = await NestFactory.create(AppModule);
+  }
+
   const config = new DocumentBuilder()
     .setTitle('Nest JS API documentation')
     .setDescription('REST NestJS API')
@@ -14,6 +31,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)  
   SwaggerModule.setup('/doc', app, document)
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+  app.useGlobalFilters(new HttpExceptionFilter)
+  await app.listen(process.env.PORT, '0.0.0.0', ()=>{
+    console.log(`App is running at ${process.env.PORT} port. App mode ${mode}`)
+  });
 }
 bootstrap();
